42895e7783170ee93f77e74168ff488646bca77e
Elpira <elpira@elpira.168.1.36>
2024-11-07 23:13:14 +0100

    1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,49 +1,79 @@
 #!/usr/bin/env python3
-"""
-Module for filtering log messages.
-"""
+"""Module for managing and logging sensitive personal data with redaction."""
 
-from typing import List
 import re
 import logging
+import mysql.connector
 from os import environ
-
-
-def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str) -> str:
-    """Returns the log message obfuscated."""
-    pattern = '|'.join([f"{field}=[^{separator}]*" for field in fields])
-    return re.sub(pattern,
-                  lambda m: f"{m.group().split('=')[0]}={redaction}", message)
-
-
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class. """
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self, fields: List[str]):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
-
-    def format(self, record: logging.LogRecord) -> str:
-        original_message = super(RedactingFormatter, self).format(record)
-        return filter_datum(self.fields,
-                            self.REDACTION, original_message, self.SEPARATOR)
-
+from typing import List
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
+def filter_datum(fields: List[str],
+                 redaction: str, message: str, separator: str) -> str:
+    """Replaces sensitive data fields in a log."""
+    pattern = f"({'|'.join(fields)})=.*?{separator}"
+    return re.sub(pattern,
+                  lambda x: f"{x.group(1)}={redaction}{separator}", message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """Formatter that redacts specified fields in log messages."""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super().__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+        return super().format(record)
+
+
 def get_logger() -> logging.Logger:
-    """Returns a logger object."""
+    """Configures and returns a logger for user data with redaction enabled."""
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
     logger.propagate = False
-    stream_handler = logging.StreamHandler()
-    formatter = RedactingFormatter(fields=PII_FIELDS)
-    stream_handler.setFormatter(formatter)
-    logger.addHandler(stream_handler)
+
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(fields=PII_FIELDS))
+    logger.addHandler(handler)
+
     return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """Creates and returns a connection to the personal data MySQL database."""
+    return mysql.connector.connect(
+        user=environ.get("PERSONAL_DATA_DB_USERNAME", "root"),
+        password=environ.get("PERSONAL_DATA_DB_PASSWORD", ""),
+        host=environ.get("PERSONAL_DATA_DB_HOST", "localhost"),
+        database=environ.get("PERSONAL_DATA_DB_NAME")
+    )
+
+
+def main():
+    """Connects to the database, retrieves and logs user data."""
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+    column_names = [col[0] for col in cursor.description]
+    logger = get_logger()
+
+    for row in cursor:
+        row_data = "; ".join(f"{name}={value}" for name,
+                             value in zip(column_names, row))
+        logger.info(row_data + ";")
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == '__main__':
+    main()
