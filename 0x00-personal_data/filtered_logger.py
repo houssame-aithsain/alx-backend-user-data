f5a2302e9bbaf69c8622737a5a39b7abdc4d683e
Elpira <elpira@elpira.168.1.36>
2024-11-07 23:35:45 +0100

    1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -17,3 +17,20 @@ def filter_datum(fields: List[str],
     pattern = f"({'|'.join(fields)})=.*?{separator}"
     return re.sub(pattern,
                   lambda x: f"{x.group(1)}={redaction}{separator}", message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """Formatter that redacts specified fields in log messages."""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super().__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+        return super().format(record)
