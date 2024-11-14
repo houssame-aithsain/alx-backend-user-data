6a4a2a8be66b06d2187e55d9f19123d98ca6f0bf
Houssame <hossame@inbox.ru>
2024-11-14 21:06:46 +0100

    app.py

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -4,6 +4,7 @@ from api.v1.views import app_views
 from flask import abort, jsonify, request
 from models.user import User
 
+
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
 def view_all_users() -> str:
     """ GET /api/v1/users
@@ -12,6 +13,7 @@ def view_all_users() -> str:
     """
     all_users = [user.to_json() for user in User.all()]
     return jsonify(all_users)
+
 
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 def view_one_user(user_id: str = None) -> str:
@@ -51,6 +53,7 @@ def delete_user(user_id: str = None) -> str:
     user.remove()
     return jsonify({}), 200
 
+
 @app_views.route('/users', methods=['POST'], strict_slashes=False)
 def create_user() -> str:
     """ POST /api/v1/users/
@@ -82,6 +85,7 @@ def create_user() -> str:
 
     except Exception as e:
         return jsonify({'error': f"Can't create User: {e}"}), 400
+
 
 @app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
 def update_user(user_id: str = None) -> str:
