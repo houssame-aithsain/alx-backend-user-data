0e8107d36d5e91027fcb8ce66114cfa6aef38088
Houssame <hossame@inbox.ru>
2024-11-14 22:56:48 +0100

    app.py

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-""" Module of Users views
+"""
+Defines routes for user authentication, including login and logout.
 """
 import os
-from flask import jsonify, request
+from flask import jsonify, request, abort
 from api.v1.views import app_views
 from models.user import User
 
@@ -10,19 +11,25 @@ from models.user import User
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def auth_session():
     """
-    Handle user login
-    Return:
-        dictionary representation of user if found else error message
+    Handles user login by verifying email and password.
+    
+    Returns:
+        JSON response containing user data if authentication is successful.
+        JSON error response if email or password is missing, user not found,
+        or password is incorrect.
     """
     email = request.form.get('email')
     password = request.form.get('password')
-    if email is None or email == '':
+    
+    if not email:
         return jsonify({"error": "email missing"}), 400
-    if password is None or password == '':
+    if not password:
         return jsonify({"error": "password missing"}), 400
+    
     users = User.search({"email": email})
-    if not users or users == []:
+    if not users:
         return jsonify({"error": "no user found for this email"}), 404
+    
     for user in users:
         if user.is_valid_password(password):
             from api.v1.app import auth
@@ -31,14 +38,18 @@ def auth_session():
             session_name = os.getenv('SESSION_NAME')
             resp.set_cookie(session_name, session_id)
             return resp
+    
     return jsonify({"error": "wrong password"}), 401
 
 
-@app_views.route('/auth_session/logout', methods=['DELETE'],
-                 strict_slashes=False)
+@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
 def handle_logout():
     """
-    Handle user logout
+    Handles user logout by destroying the session.
+    
+    Returns:
+        200 status code with empty JSON if logout is successful.
+        404 error if session cannot be destroyed.
     """
     from api.v1.app import auth
     if auth.destroy_session(request):
