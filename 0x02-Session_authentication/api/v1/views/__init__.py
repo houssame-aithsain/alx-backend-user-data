f0555192c2c214de4f1df4cb191f4f2ea221e398
Houssame <hossame@inbox.ru>
2024-11-14 22:54:00 +0100

    app.py

--- 0x02-Session_authentication/api/v1/views/__init__.py
+++ 0x02-Session_authentication/api/v1/views/__init__.py
@@ -3,14 +3,12 @@
 Defines the app_views Blueprint for organizing the API routes.
 """
 from flask import Blueprint
-
-# Blueprint for API version 1 routes with a URL prefix of /api/v1
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
-
-# Importing views to register routes with the Blueprint
 from api.v1.views.index import *
 from api.v1.views.users import *
 from api.v1.views.session_auth import *
+
+# Blueprint for API version 1 routes with a URL prefix of /api/v1
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 # Load users from file storage upon initialization
 User.load_from_file()
