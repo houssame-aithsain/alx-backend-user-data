2a06ff3ce0a8bf3cafad954345bc647d777b436b
Houssame <hossame@inbox.ru>
2024-11-21 17:19:14 +0100

    0

--- 0x03-user_authentication_service/README.md
+++ 0x03-user_authentication_service/README.md
@@ -1,1 +1,15 @@
-test0
+# User Authentication Service
+
+This project implements a user authentication service using Python 3.7 and SQLAlchemy 1.3.x. The service provides functionality to:
+- Define a `User` model.
+- Add users to the database.
+- Query users with error handling.
+
+## Requirements
+- Python 3.7
+- SQLAlchemy 1.3.x
+- SQLite database
+- bcrypt for password hashing
+
+## Usage
+Run the `main.py` script to test the implementation.
--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,53 +1,69 @@
 #!/usr/bin/env python3
-"""DB module
 """
+DB module for interacting with the database.
+"""
+
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
-from user import User
-from sqlalchemy.exc import InvalidRequestError, NoResultFound
-from user import Base
+from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+from user import Base, User
 
 
 class DB:
-    """DB class
+    """
+    Handles interactions with the database.
     """
 
     def __init__(self) -> None:
-        """Initialize a new DB instance
-        """
-        self._engine = create_engine("sqlite:///a.db", echo=False)
+        """Initializes the DB instance."""
+        self._engine = create_engine("sqlite:///a.db", echo=True)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
     def _session(self) -> Session:
-        """Memoized session object
-        """
+        """Creates or retrieves the session."""
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """create new user"""
-        session = self._session
-        user = User(email=email, hashed_password=hashed_password)
-        session.add(user)
-        session.commit()
-        return user
+        """
+        Adds a new user to the database.
 
-    def find_user_by(self, **kwargs):
-        if len(kwargs) != 1:
+        Args:
+            email (str): The user's email.
+            hashed_password (str): The hashed password.
+
+        Returns:
+            User: The created user.
+        """
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
+        self._session.commit()
+        return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        Finds a user by the given criteria.
+
+        Args:
+            **kwargs: Arbitrary keyword arguments for filtering.
+
+        Returns:
+            User: The first matching user.
+
+        Raises:
+            NoResultFound: If no user matches the criteria.
+            InvalidRequestError: If invalid query arguments are passed.
+        """
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+            if not user:
+                raise NoResultFound
+            return user
+        except TypeError:
             raise InvalidRequestError
-        
-        fieldName, value = list(kwargs.items())[0]
-        for atr in User.__dir__():
-            if atr == fieldName:
-                try:
-                    User(fieldName=value)
-                except Exception as a:
-                    raise NoResultFound
-        raise InvalidRequestError
